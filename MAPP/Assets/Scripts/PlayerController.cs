ffaeb903a94aa25502b4a215d072a933d19e3e00
Arbuzee <arbuzemail@gmail.com>
2018-03-27 22:39:11 +0200

    Camera/ Player/ Hook
    * Added some sprites for development purposes
    * Added environment with colliders for development purposes
    * Added camera follow script
    * Added player script
    * Added first attempt at grappling hook (distance joint 2D)

---
+++ MAPP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public DistanceJoint2D joint;
+    public Vector2 v2;
+
+    public Vector2 playerpos;
+
+    private float hookSpeed = 3;
+    private float distance = 0;
+    private float pullSpeed = 1;
+
+	void Start () {
+        // Safetycheck, makes sure a reference to joint exists
+        if (joint == null)
+        {
+            joint = this.GetComponent<DistanceJoint2D>();
+        }
+
+        // Resets joint.distance
+        joint.distance = 0;
+
+
+
+    }
+	
+	void Update () {
+        // Throw hook on mouseButtonDown
+		if (Input.GetMouseButtonDown(0))
+        {
+            ThrowGrapplingHook();
+        }
+
+        PullPlayerToHook();
+
+
+    }
+
+    private void PullPlayerToHook()
+    {
+        // Reduces joint.distance over time
+        // Meant to pull the player towards the hook
+        joint.distance -= pullSpeed * Time.deltaTime;
+    }
+
+    private void ThrowGrapplingHook()
+    {
+        // Sets hook anchor position to mousePosition
+        joint.connectedAnchor = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+    }
+}
