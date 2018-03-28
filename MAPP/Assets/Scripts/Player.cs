80e9eb3f9343a6f35f5abebea1826069050917d8
Alexander Castman <alexander.castman@gmail.com>
2018-03-28 19:55:05 +0200

    Desperat försök att få mekaniken att fungera
    Jag har avaktiverat den första spelaren som du skapat och börjat på ett nytt script för att testa om det gick att lösa på ett annat sätt. Ingen stor framgång än dock

---
+++ MAPP/Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject target;
+    GameObject currentTarget;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        checkMouseClick();
+        launchPlayerAgainstHook();
+
+	}
+
+
+    void checkMouseClick() {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+          
+             createGrapplingHookTarget();
+            
+
+           
+        }
+    
+    }
+
+
+    void createGrapplingHookTarget(){
+
+        Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        currentTarget = (GameObject) Instantiate(target, clickPosition, Quaternion.identity);
+        currentTarget.GetComponent<Rope>().target = clickPosition;
+
+    }
+
+
+    void launchPlayerAgainstHook(){
+
+        if (currentTarget != null)
+        {
+
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            
+            //transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, 0.5f);
+
+
+            rb.AddForce(currentTarget.transform.position - transform.position * 100);
+        
+        }
+        Destroy(currentTarget);
+    }
+
+
+}
---
+++ MAPP/Assets/Scripts/Rope.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour {
+
+    public Vector2 target;
+    public float speed = 1;
+
+	private void Update()
+	{
+        setHookPosition();
+
+
+	}
+
+
+    void setHookPosition(){
+
+        transform.position = Vector2.MoveTowards (transform.position, target, speed);
+    }
+
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.CompareTag("Player")){
+
+            Destroy(gameObject);
+        }
+	}
+
+}
