539d3026cf13d470e9bd641e3bf0a2cf567ed162
alexancast <37774672+alexancast@users.noreply.github.com>
2018-04-05 10:38:52 +0200

    Ny collider för att undvika bullet->paper

--- MAPP/Assets/Scripts/Rope.cs
+++ MAPP/Assets/Scripts/Rope.cs
@@ -32,7 +32,8 @@ public class Rope : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
 	{
-        if (collision.CompareTag("Player")){
+        if (collision.CompareTag("Terrain")){
+
 
 
         }
