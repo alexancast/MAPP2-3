1dc09852d0cfb371beb558a75ede7c03ecd8c0c0
Arbuzee <arbuzemail@gmail.com>
2018-04-24 14:59:26 +0200

    HeightMeter

---
+++ MAPP/Assets/Scripts/HeightMeter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeightMeter : MonoBehaviour {
+
+    public Text text;
+    public float heightConverter;
+    public float heightOffset;
+
+    private GameObject player;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+
+	void Update () {
+        float height = player.transform.position.y;
+        height = (int)height * heightConverter - heightOffset;
+        text.text = height.ToString() + "m";
+	}
+}
