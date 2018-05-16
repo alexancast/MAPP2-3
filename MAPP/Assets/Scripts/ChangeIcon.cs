bd204aa078f25de29b7d2d534d37dfa3fdfb0a90
Arbuzee <37773311+Arbuzee@users.noreply.github.com>
2018-05-16 21:55:04 +0200

    UI Improvements
    * Made buttons look nicer
    * Added on and off icons for sound and music buttons
    * These icons change on use of button

---
+++ MAPP/Assets/Scripts/ChangeIcon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeIcon : MonoBehaviour {
+
+    public Image image;
+    public Sprite image1;
+    public Sprite image2;
+
+    private void Start()
+    {
+        //image = GetComponent<Image>();
+    }
+
+    public void ChangeSprite()
+    {
+        if (image.sprite != image1)
+        {
+            image.sprite = image1;
+        } else
+        {
+            image.sprite = image2;
+        }
+        
+    }
+}
