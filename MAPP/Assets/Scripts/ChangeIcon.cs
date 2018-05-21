b25cec16751ad1bfe0d1fd4e00be5bde06799657
alexancast <37774672+alexancast@users.noreply.github.com>
2018-05-21 12:09:48 +0200

    Music Mute Button works

--- MAPP/Assets/Scripts/ChangeIcon.cs
+++ MAPP/Assets/Scripts/ChangeIcon.cs
@@ -8,21 +8,58 @@ public class ChangeIcon : MonoBehaviour {
     public Image image;
     public Sprite image1;
     public Sprite image2;
+	private GameObject camera;
 
     private void Start()
     {
         //image = GetComponent<Image>();
+		camera = GameObject.Find("Main Camera");
+		if (PlayerPrefs.GetInt ("mute") == 1 && gameObject.name == "MusicMuteButton") {
+		
+			image.sprite = image1;
+		
+		} else if(PlayerPrefs.GetInt ("mute") == -1 && gameObject.name == "MusicMuteButton")
+			image.sprite = image2;
+
+
+
+
     }
 
     public void ChangeSprite()
     {
         if (image.sprite != image1)
         {
+			//UnMute
             image.sprite = image1;
+			UnMute ();
+
+
+
         } else
         {
             image.sprite = image2;
+			Mute ();
         }
         
     }
+
+	public void UnMute(){
+
+		PlayerPrefs.SetInt("mute", 1);
+		Debug.Log("Un Mute");
+		camera.GetComponent<AudioSource> ().mute = false;
+
+	
+	}
+
+
+	public void Mute(){
+
+	PlayerPrefs.SetInt ("mute", -1);
+	Debug.Log ("Mute");
+	camera.GetComponent<AudioSource> ().mute = true;
+
+	}
+
 }
---
+++ MAPP/Assets/Scripts/InGameSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameSound : MonoBehaviour {
+
+	private GameObject camera;
+
+	void Start () {
+
+		camera = GameObject.Find ("Main Camera");
+
+		if (PlayerPrefs.GetInt ("mute") == -1) {
+		
+			camera.GetComponent<AudioSource> ().mute = true;
+		
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
